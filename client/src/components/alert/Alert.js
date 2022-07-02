cd8111603ba5c01f4f0c3f782b77188ce2fcc110
specture-01 <101566677+specture-01@users.noreply.github.com>
2022-07-03 07:36:36 +0800

    able to login using redux

---
+++ client/src/components/alert/Alert.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { GLOBAL_TYPES } from '../../redux/actions/globalTypes'
+import Loading from './loading'
+import Toast from './Toast'
+
+const Alert = () => {
+  const dispatch = useDispatch();
+  const { alert } = useSelector((state) => state)
+  const handleShowNotify = () => {
+    dispatch({type: GLOBAL_TYPES.ALERT, payload: {}})
+  }
+  return (
+    <div>
+      {alert.loading && <Loading />}
+      {alert.error && (
+        <Toast msg={{ title: 'Oops..', body: alert.error }} handleShow={handleShowNotify} bgColor='bg-danger' />
+      )}
+      {alert.success && <Toast msg={{ title: 'Success', body: alert.success }} handleShow={handleShowNotify} bgColor='bg-success' />}
+    </div>
+  )
+}
+
+export default Alert
