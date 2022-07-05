d0b32a80c0535c88cc2564a023203b0a9bb92b8f
specture-01 <101566677+specture-01@users.noreply.github.com>
2022-07-05 19:05:44 +0800

    user info profile

---
+++ client/src/components/profile/Info.js
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { useParams } from 'react-router-dom'
+import { getProfileUser } from '../../redux/actions/profileAction'
+import Avatar from '../Avatar'
+
+const Info = () => {
+  const { id } = useParams()
+  const { auth, profile } = useSelector(state => state)
+  const dispatch = useDispatch()
+
+  const [userData, setUserData] = useState([])
+
+  useEffect(() => {
+    if (id === auth.user._id) {
+      setUserData([auth.user])
+    } else {
+      dispatch(getProfileUser({users: profile.users, id, auth}))
+      const newData = profile.users.filter((user) => user._id === id);
+      console.log('newData', newData);
+      setUserData(newData);
+    }
+  }, [id, auth, dispatch, profile.users])
+
+  return (
+    <div className='info'>
+      {userData.map(user => (
+        <div className='info_container' key={user._id}>
+          <Avatar size={'supper-avatar'} src={user.avatar} />
+          <div className='info_content'>
+            <div className='info_content_title'>
+              <h2>{user.username}</h2>
+              <button className='btn btn-outline-info'>Edit Profile</button>
+            </div>
+            <div className='follow_btn'>
+              <span className='mr-4'>{user.followers.length} Followers</span>
+              <span className='ml-4'>{user.following.length} Following</span>
+            </div>
+            
+            <h6>{user.fullname} | {user.mobile}</h6>
+            <p className='m-0'>{user.address}</p>
+            <h6>{user.email}</h6>
+            
+            <a href={user.website} target='_blank' rel='noreferrer'>{user.website}</a>
+            <p>{user.story}</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default Info
