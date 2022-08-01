6db408a12d3e7d6ca7db5397644b2102c2428b33
specture-01 <101566677+specture-01@users.noreply.github.com>
2022-08-01 10:20:08 +0800

    handle save posts

--- client/src/components/home/post_card/CardFooter.js
+++ client/src/components/home/post_card/CardFooter.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import {Link} from 'react-router-dom'
 import Send from '../../../images/send.svg'
-import { likePost, unLikePost } from '../../../redux/actions/postAction'
+import { likePost, savePost, unLikePost, unSavePost } from '../../../redux/actions/postAction'
 import { BASE_URL } from '../../../utils/config'
 import LikeButton from '../../LikeButton'
 import ShareModal from '../../ShareModal'
@@ -14,29 +14,56 @@ const CardFooter = ({post}) => {
   const [isLike, setIsLike] = useState(false)
   const [loadLike, setLoadLike] = useState(false)
   const [isShare, setIsShare] = useState(false)
+  const [saved, setSaved] = useState(false)
+  const [saveLoad, setSaveLoad] = useState(false)
 
   useEffect(() => {
     if(post.likes.find(like => like._id === auth.user._id)) {
       setIsLike(true)
+    } else {
+      setIsLike(false)
     }
   }, [post.likes, auth.user._id])
 
   const handleLike = async () => {
     if(loadLike) return;
-    setIsLike(true)
     
     setLoadLike(true)
     await dispatch(likePost({post, auth}))
     setLoadLike(false)
   }
+
   const handleUnLike = async() => {
     if(loadLike) return;
-    setIsLike(false)
 
     setLoadLike(true)
     await dispatch(unLikePost({post, auth}))
     setLoadLike(false)
   }
+
+  useEffect(() => {
+    if(auth.user.saved.find(id => id === post._id)) {
+      setSaved(true)
+    } else {
+      setSaved(false)
+    }
+  },[auth.user.saved, post._id])
+
+  const handleSavePost = async () => {
+     if(saveLoad) return;
+
+    setSaveLoad(true)
+    await dispatch(savePost({post, auth}))
+    setSaveLoad(false)
+  }
+  const handleUnSavePost = async() => {
+     if(saveLoad) return;
+
+     setSaveLoad(true)
+    await dispatch(unSavePost({post, auth}))
+    setSaveLoad(false)
+  }
+
   return (
     <div className='card_footer'>
       <div className='card_icon_menu'>
@@ -52,7 +79,11 @@ const CardFooter = ({post}) => {
           </Link>
           <img src={Send} alt="send" onClick={() => setIsShare(!isShare)} />
         </div>
-        <i className='far fa-bookmark' />
+        {
+          saved 
+          ? <i className='fas fa-bookmark text-info' onClick={handleUnSavePost} />
+          : <i className='far fa-bookmark' onClick={handleSavePost}/>
+        }
       </div>
       <div className='d-flex justify-content-between'>
         <h6 style={{padding: '0 25px', cursor: 'pointer'}}>{post.likes.length} likes</h6>
